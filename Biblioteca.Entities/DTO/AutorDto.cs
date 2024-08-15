33976d2e6efc9e07a3dffb3358378e9aa8a7de78
Karen Rivas <72423267+Haruki1707@users.noreply.github.com>
2024-08-14 18:07:23 -0600

    Initial commit

---
+++ Biblioteca.Entities/DTO/AutorDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Entities.DTO
+{
+    public class AutorDto
+    {
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "El nombre del autor es requerido")]
+        [StringLength(50, ErrorMessage = "El nombre del autor no puede exceder los 50 caracteres")]
+        public string NombreAutor { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "El apellido del autor es requerido")]
+        [StringLength(50, ErrorMessage = "El apellido del autor no puede exceder los 50 caracteres")]
+        public string ApellidoAutor { get; set; } = String.Empty;
+    }
+}
